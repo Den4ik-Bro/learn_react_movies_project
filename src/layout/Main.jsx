ef67d505bac05957b037b7e9e2fee2d6f42e8bbe
Den4ik-Bro <denis.rr@bk.ru>
2024-02-26 18:28:59 +0300

    remove old comments , code format

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,20 +4,19 @@ import { Movies } from "../components/Movies";
 import { Preloader } from "../components/Preloader";
 import { Search } from "../components/Search";
 
-
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-
 export const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [movies, setMovies] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-
-  const handleSearch = (str, type='all') => {
-    console.log(str, type)
+  const handleSearch = (str, type = "all") => {
+    console.log(str, type);
     setIsLoading(true);
     fetch(
-      `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`
+      `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${
+        type !== "all" ? `&type=${type}` : ""
+      }`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -30,38 +29,22 @@ export const Main = () => {
   };
 
   useEffect(() => {
-    console.log('useEffect')
-    fetch(
-      `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=terminator`
-    ).then((res) => res.json())
-     .then((data) => {
-      setMovies(data.Search);
-      setIsLoading(false)
-    })
-    .catch((error) => {
-      console.error("Movies load error", error);
-    })
+    console.log("useEffect");
+    fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=terminator`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.Search);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Movies load error", error);
+      });
   }, []);
 
-  // componentDidMount() {
-  //   fetch(
-  //     `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=terminator`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       this.setState({ movies: data.Search, isLoading: false });
-  //     })
-  //     .catch((error) => {
-  //       console.error("Movies load error", error);
-  //     });
-  // }
-
-    return (
-      <main className="container content">
-        <Search handleSearch={handleSearch} />
-        {isLoading ? <Preloader /> : <Movies movies={movies} />}
-      </main>
-    );
-  }
-
-
+  return (
+    <main className="container content">
+      <Search handleSearch={handleSearch} />
+      {isLoading ? <Preloader /> : <Movies movies={movies} />}
+    </main>
+  );
+};
